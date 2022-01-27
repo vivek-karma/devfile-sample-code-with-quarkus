6aa0ecb2bbf2c419f04719309b106d0e68a5d73c
vivek-karma <41522395+vivek-karma@users.noreply.github.com>
2022-01-27 12:55:44 +0530

    Update GreetingResource.java

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -5,12 +5,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello-resteasy")
+@RestController
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+   @PostMapping("/")
+    public String getFeedback(@RequestBody Feedback feedback) {
+        return "Thanks for submitting feedback!";
     }
 }
